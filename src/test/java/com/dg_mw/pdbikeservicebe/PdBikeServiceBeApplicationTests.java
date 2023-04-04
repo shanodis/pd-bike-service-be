dcee3c1f7850ba2ef8b3beac076a2f3221adbd39
Dominik <dominik.grudzien00@gmail.com>
2023-04-04 23:06:56 +0200

    initial commit

---
+++ src/test/java/com/dg_mw/pdbikeservicebe/PdBikeServiceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dg_mw.pdbikeservicebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdBikeServiceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
