77489981e3887d0e2ff7e0f3893053e6a642ed7b
Dominik <dominik.grudzien00@gmail.com>
2023-04-15 15:04:27 +0200

    swagger config

---
+++ src/main/java/me/project/config/SwaggerConfiguration.java
@@ -0,0 +1,41 @@
+package me.project.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Configuration
+@RestController
+@RequestMapping(path = "/")
+public class SwaggerConfiguration {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("entire-public")
+                .pathsToMatch("/**")
+                .pathsToExclude("/")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("API v1")
+                        .description("Simple API")
+                        .version("v0.2")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
+
+    @GetMapping
+    public RedirectView redirectView(){
+        return new RedirectView("/swagger-ui.html");
+    }
+}
